9d80471834a990c65a59a4e574e9389bc9ea7afb
github <github@server.10me.com>
2015-07-21 12:29:02 +0200

    third part test

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,7 +5,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!!!!</h1>
+        <h1>Congratulations!!!! Yii2 it's awesome!</h1>
 
         <p class="lead">You have successfully created your Yii2-powered application.</p>
 
